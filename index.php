f2d0ac365e1f16c9105c8617f255823a4dcff04c
Aurelia BLONDEL <aureliablondel56@gmail.com>
2021-01-28 15:18:22 +0100

    second

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
+// on charge en premier le fichier autoload
+require('vendor/autoload.php');
 
-echo('Hello world!');
+// si mon url est différente de l'url en cours (HTTP_HOST), cad que je ne suis pas en ligne, heroku charge le fichier .env
+// .env a permis de stocker le token pour qu'il ne soit pas accessible
+// ca permet de cacher la clé qd mon appli est en ligne
+
+if($_SERVER['HTTP_HOST'] != "api-slack-aure.herokuapp.com"){
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+}
+
+$ary = array('token' => $_ENV['TOKEN']);
+$data = json_encode($ary); // transforme donnée en json
+
+echo($data);
+
